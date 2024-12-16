a134edeffcccfc32c48be504394fac1c97a3b7a2
Nyte <info@nytetv.de>
2024-12-16 15:55:21 +0100

    YouTube-Button zu Game-Cards hinzugefügt

--- archive-game.php
+++ archive-game.php
@@ -30,6 +30,7 @@ get_header(); ?>
                     $platform = get_post_meta(get_the_ID(), 'game_platform', true);
                     $is_game_of_month = get_post_meta(get_the_ID(), 'game_of_month', true);
                     $status = get_post_meta(get_the_ID(), 'game_status', true);
+                    $youtube_url = get_post_meta(get_the_ID(), 'game_youtube_url', true);
                     ?>
                     <div class="game-card <?php echo esc_attr($status); ?>" data-aos="fade-up">
                         <div class="game-image">
@@ -57,6 +58,13 @@ get_header(); ?>
                                     }
                                     ?>
                                 </span>
+                            <?php endif; ?>
+                            <?php if ($youtube_url) : ?>
+                                <a href="<?php echo esc_url($youtube_url); ?>" class="youtube-btn" target="_blank" rel="noopener">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                        <path fill="currentColor" d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+                                    </svg>
+                                </a>
                             <?php endif; ?>
                         </div>
                         <div class="game-info">
@@ -94,7 +102,7 @@ get_header(); ?>
                                         <?php foreach ($custom_fields as $key => $values) :
                                             // Überspringe interne WordPress-Felder, bereits angezeigte Felder und Ultpgrid-Felder
                                             if (substr($key, 0, 1) === '_' || 
-                                                in_array($key, ['game_progress', 'game_platform', 'game_of_month', 'game_status']) ||
+                                                in_array($key, ['game_progress', 'game_platform', 'game_of_month', 'game_status', 'game_youtube_url']) ||
                                                 strpos($key, 'ultpgrid') !== false) continue;
                                             ?>
                                             <li>
